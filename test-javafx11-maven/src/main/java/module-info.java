0578957f5bbeb50614fce09962e79471f566b72e
sansey <sansey@sansey-pc>
2019-07-13 09:57:42 +0300

    Initial commit

---
+++ test-javafx11-maven/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module ru.hse.test_javafx11_maven {
+    requires javafx.controls;
+    exports ru.hse.test_javafx11_maven;
+}
---
+++ test-javafx11-maven/src/main/java/ru/hse/test_javafx11_maven/SystemInfo.java
@@ -0,0 +1,13 @@
+package ru.hse.test_javafx11_maven;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
